5ab5a52bf56f23b01d97387ccada485ee25f78ce
Unmesh Deodhar <unmesh@ada.cs.pdx.edu>
2018-01-20 13:59:09 -0800

    Started writing packet class.

--- Project1/src/Constants.h
+++ Project1/src/Constants.h
@@ -1,2 +1,12 @@
 const int PORT_NUMBER = 2939;	//This will be our default port for our protocol.
 #define HOSTNAME "ada" //Because we are always going to run on linux lab machines.
+
+typedef enum packetKind {
+	CONNECTION_REQUEST,
+	CONNECTION_ACK,
+	FILE_REQUEST,
+	FILE_REQUEST_ACK,
+	DATA,
+	DATA_ACK,
+	CLOSE_CONNECTION,
+};
