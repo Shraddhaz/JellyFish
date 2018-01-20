5f875ee9cff6b1985e627e876a158870ac366d41
Shraddha Bhanudas Zingade <shraddha@babbage.cs.pdx.edu>
2018-01-20 11:59:06 -0800

    Modifying RTFPServer

--- Project1/src/RFTPServer.h
+++ Project1/src/RFTPServer.h
@@ -1,5 +1,18 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <netdb.h>
+#include <stdio.h>
+
 class RFTPServer {
-	private: 
+	private:
+		int sock, length, n;
+   		socklen_t fromlen;
+   		struct sockaddr_in server;
+   		struct sockaddr_in from; 
 	public:
 		void listen();
 		
